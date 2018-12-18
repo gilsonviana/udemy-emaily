748e19e9a7111f4c57f3bdc110b3120f4a36af11
gilsonviana <vianawebdev@gmail.com>
2018-12-18 08:20:45 -0500

    :construction: axios library and redux-thunk wired up

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Header from './Header'
+
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 export default class App extends Component {
   render() {
     return(
-      <div className="app">
-        Hello
+      <div className="app container">
+        <Header />
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </Switch>
+        </BrowserRouter>
       </div>
     )
   }
